0be95ce7a7dacf1cdbf79e1e8376cd54c1f2e7aa
Flavien <you@example.com>
2025-11-28 15:29:19 +0500

    - manga controller

--- backend/app/presentation/api/manga_controller.py
+++ backend/app/presentation/api/manga_controller.py
@@ -5,3 +5,20 @@ router = APIRouter(prefix="/manga")
 @router.get("/{id}")
 async def get_manga_by_id():
     pass
+
+@router.get("/{id}/chapters")
+async def get_manga_chapters_by_id():
+    pass
+
+@router.get("/{id}/chapters/{chapter_id}")
+async def get_manga_chapter_by_id():
+    pass
+
+@router.get("/{id}/chapters/{chapter_id}/pages")
+async def get_manga_chapter_pages_by_id():
+    pass
+
+@router.get("/{id}/chapters/{chapter_id}/pages/{page_id}")
+async def get_manga_chapter_page_by_id():
+    pass
+
